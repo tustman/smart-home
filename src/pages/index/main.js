cf9a7ef5182dcb011b1cc589ff7cc4f89716e936
wangyining <wangyining@ixiaocong.com>
2018-06-15 13:56:42 +0800

    初始化项目

---
+++ src/pages/index/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './index'
+
+const app = new Vue(App)
+app.$mount()
+export default {
+  config: {
+    'usingComponents': {
+      'i-button': '../../static/button/index'
+    }
+  }
+}
